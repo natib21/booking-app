8275c01d8bcf7cde77c5a59f92987da78a56a321
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-11-27 00:17:11 -0800

    add server side form validation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/natib21/bookings/internal/config"
+	"github.com/natib21/bookings/internal/forms"
 	"github.com/natib21/bookings/internal/models"
 	"github.com/natib21/bookings/internal/render"
 	"log"
@@ -24,11 +25,13 @@ func NewRepo(a *config.AppConfig) *Repository {
 func NewHandlers(r *Repository) {
 	Repo = r
 }
+
 func (m *Repository) Home(res http.ResponseWriter, req *http.Request) {
 	remoteIp := req.RemoteAddr
 	m.App.Session.Put(req.Context(), "remote_ip", remoteIp)
 	render.RenderTemplate(res, req, "home.page.gohtml", &models.TemplateData{})
 }
+
 func (m *Repository) About(res http.ResponseWriter, req *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello,again"
@@ -42,9 +45,52 @@ func (m *Repository) About(res http.ResponseWriter, req *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
 func (m *Repository) Reservation(res http.ResponseWriter, req *http.Request) {
-	render.RenderTemplate(res, req, "make-reservation.page.gohtml", &models.TemplateData{})
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
+	render.RenderTemplate(res, req, "make-reservation.page.gohtml", &models.TemplateData{
+		Form: forms.New(nil),
+		Data: data,
+	})
+
 }
+func (m *Repository) PostReservation(res http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	reservation := models.Reservation{
+		FirstName: req.Form.Get("first_name"),
+		LastName:  req.Form.Get("last_name"),
+		Phone:     req.Form.Get("phone"),
+		Email:     req.Form.Get("email"),
+	}
+
+	form := forms.New(req.PostForm)
+
+	//form.Has("first_name", req)
+
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, req)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(res, req, "make-reservation.page.gohtml", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+		return
+
+	}
+}
+
 func (m *Repository) Generals(res http.ResponseWriter, req *http.Request) {
 	render.RenderTemplate(res, req, "generals.page.gohtml", &models.TemplateData{})
 }
@@ -52,9 +98,11 @@ func (m *Repository) Generals(res http.ResponseWriter, req *http.Request) {
 func (m *Repository) Majors(res http.ResponseWriter, req *http.Request) {
 	render.RenderTemplate(res, req, "majours.page.gohtml", &models.TemplateData{})
 }
+
 func (m *Repository) Availibility(res http.ResponseWriter, req *http.Request) {
 	render.RenderTemplate(res, req, "search-availability.page.gohtml", &models.TemplateData{})
 }
+
 func (m *Repository) PostAvailibility(res http.ResponseWriter, req *http.Request) {
 	start := req.Form.Get("start")
 	end := req.Form.Get("end")
